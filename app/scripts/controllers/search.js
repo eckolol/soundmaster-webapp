cfb185b5e760f9233f885f27d4727eafa4e8e54d
Jerome Botineau <jeromebotineau@gmail.com>
2014-05-24 23:40:44 +0200

    Add search Youtube controller

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -1,6 +1,28 @@
 'use strict';
 
 angular.module('soundMaster')
-  .controller('SearchCtrl', function ($scope) {
+  .controller('SearchCtrl', function ($scope, $http, soundSocket) {
+
+  $scope.search = function() {
+    var search = $scope.searchSong;
+    if(search.length>3) {
+      var dataurl ='http://gdata.youtube.com/feeds/api/videos?alt=json&q='+search;
+ 
+      $http.get(dataurl).
+        success(function(data, status, headers, config) {
+          $scope.videos=data.feed.entry;
+        }).
+        error(function(data, status, headers, config) {
+          console.log('Error');
+        });
+    }
+  };
+
+  $scope.addSong = function(id) {
+    id = id.substring(42);
+    soundSocket.emit('playStream', {
+      type: 'youtube', stream: id
+    });
+  };
 
   });
